5bc277f9314601652cdd3de48840a017feb40e61
peter <krothu@outlook.com>
2023-02-14 11:37:34 +0000

    Initial commit of API

---
+++ theDoSomethingService/Controllers/DoSomething.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace theDoSomethingService.Controllers;
+
+public class DoSomething : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
