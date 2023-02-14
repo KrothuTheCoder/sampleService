6b95b9328fba27f8ee9db5bb811cb9b90be73e3a
peter <krothu@outlook.com>
2023-02-14 16:04:45 +0000

    changed the project

---
+++ DoSomethingService/Controllers/SomethingController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoSomethingService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SomethingController : ControllerBase
+{
+    private readonly ILogger<SomethingController> _logger;
+
+    public SomethingController(ILogger<SomethingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "DoSomething")]
+    public string Get(string input)
+    {
+        return $"This was passed to me {input}, there I did something";
+    }
+}
