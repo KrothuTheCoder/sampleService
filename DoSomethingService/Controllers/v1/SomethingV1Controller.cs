284c0911fe5ba8ce6ef08a39f4c310396c282a78
peter <krothu@outlook.com>
2024-02-21 15:48:13 +0000

    Adding a new version

--- DoSomethingService/Controllers/v1/SomethingV1Controller.cs
+++ DoSomethingService/Controllers/v1/SomethingV1Controller.cs
@@ -7,6 +7,7 @@ namespace DoSomethingService.Controllers.v1;
 /// </summary>
 [ApiController]
 [ApiVersion("1.0")]
+[ApiVersion("2.0")]
 [Route("api/v{version:apiVersion}/[controller]")]
 public class SomethingController : ControllerBase
 {
@@ -22,6 +23,16 @@ public class SomethingController : ControllerBase
     }
 
     /// <summary>
+    /// If you need to do something then it will
+    /// </summary>
+    /// <returns>What happened</returns>
+    [HttpGet("DoSomething")]
+    public string Get()
+    {
+        return $"You wanted me to do something, there I did something";
+    }
+
+    /// <summary>
     /// If you need to do something then, just tell it
     /// what you want to do
     /// </summary>
@@ -32,7 +43,23 @@ public class SomethingController : ControllerBase
     {
         return $"You wanted me to {whatToDo}, there I did something";
     }
-    
+    /// <summary>
+    /// If you need to do something then, just tell it
+    /// what you want to do
+    /// </summary>
+    /// <param name="whatToDo">The thing you want it to do</param>
+    /// <param name="numberOfTimes">The number of times you want it to do something</param>
+    /// <returns>What happened</returns>
+    [MapToApiVersion("2.0")]
+    public string Get(string whatToDo, int numberOfTimes)
+    {
+        var returnString = "";
+        for (int i = 0; i < numberOfTimes; i++)
+        {
+            returnString += $"You wanted me to {whatToDo}, there I did something";
+        }
+        return returnString;
+    }
     /// <summary>
     /// If you want to post something for it to do
     /// </summary>
