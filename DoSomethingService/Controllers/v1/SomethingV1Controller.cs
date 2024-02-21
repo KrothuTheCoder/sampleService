5268c0653e2d57ea3db1fff836769a72fab52255
peter <krothu@outlook.com>
2024-02-21 14:28:52 +0000

    more handling of swagger and appinstights logging

--- DoSomethingService/Controllers/v1/SomethingV1Controller.cs
+++ DoSomethingService/Controllers/v1/SomethingV1Controller.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DoSomethingService.Controllers.v1;
 
 /// <summary>
-/// 
+/// This is an api to return some data in a made up senario
 /// </summary>
 [ApiController]
 [Route("v1/something")]
@@ -12,33 +12,34 @@ public class SomethingV1Controller : ControllerBase
     private readonly ILogger<SomethingV1Controller> _logger;
 
     /// <summary>
-    /// 
+    /// Setting up the code so it can do things
     /// </summary>
-    /// <param name="logger"></param>
+    /// <param name="logger">The logger so we know if things have been done</param>
     public SomethingV1Controller(ILogger<SomethingV1Controller> logger)
     {
         _logger = logger;
     }
 
     /// <summary>
-    /// 
+    /// If you need to do something then, just tell it
+    /// what you want to do
     /// </summary>
-    /// <param name="input"></param>
-    /// <returns></returns>
+    /// <param name="whatToDo">The thing you want it to do</param>
+    /// <returns>What happened</returns>
     [HttpGet("DoSomething")]
-    public string Get(string input)
+    public string Get(string whatToDo)
     {
-        return $"This was passed to me {input}, there I did something";
+        return $"You wanted me to {whatToDo}, there I did something";
     }
     
     /// <summary>
-    /// 
+    /// If you want to post something for it to do
     /// </summary>
-    /// <param name="input"></param>
-    /// <returns></returns>
+    /// <param name="whatToDo">The thing you want it to do</param>
+    /// <returns>What happened</returns>
     [HttpPost("PostSomething")]
-    public string Post(string input)
+    public string Post(string whatToDo)
     {
-        return $"This was passed to me {input}, there I did something";
+        return $"This was passed to me {whatToDo}, there I did something";
     }
 }
--- DoSomethingService/Program.cs
+++ DoSomethingService/Program.cs
@@ -2,7 +2,11 @@ using Microsoft.OpenApi.Models;
 
 var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry();
+
 var apiVersions = new Dictionary<string, string>();
+
 apiVersions.Add("v1", "SomethingV1");
 apiVersions.Add("v2", "SomethingV2");
 builder.Services.AddCors(options =>
@@ -18,42 +22,38 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-/*options =>
-{
-    /*options.SwaggerDoc("v1", new OpenApiInfo
+builder.Services.AddSwaggerGen(
+    options =>
     {
-        Version = "v1",
-        Title = "The DoSomething API"
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "The DoSomething API"
+        });
+        /*options.SwaggerDoc("v2", new OpenApiInfo
+       {
+           Version = "v2",
+           Title = "The DoSomething API"
+       });*/
+        options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
     });
-    options.SwaggerDoc("v2", new OpenApiInfo
-    {
-        Version = "v2",
-        Title = "The DoSomething API"
-    });#1#
-    //options.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-});*/
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
+{
+    //options.SwaggerEndpoint($"/v1/Something/swagger/swagger.json", "SomethingV1");
+    //options.SwaggerEndpoint($"/swagger/v2/Something/swagger.json", "SomethingV2");
 
-/*options =>*/
-    /*{
-        options.SwaggerEndpoint($"/v1/Something/swagger/swagger.json", "SomethingV1");
-        options.SwaggerEndpoint($"/swagger/v2/Something/swagger.json", "SomethingV2");
-    }*/
-    /*options =>
+    // build a swagger endpoint for each discovered API version
+    foreach (var description in apiVersions)
     {
-        // build a swagger endpoint for each discovered API version
-        foreach (var description in apiVersions)
-        {
-            var name = description.Value.ToUpperInvariant();
-            var url = $"/swagger/{description.Key}/Something/swagger.json";
-            options.SwaggerEndpoint(url, name);
-        }
-    }*/
+        var name = description.Value.ToUpperInvariant();
+        var url = $"/swagger/{description.Key}/Something/swagger.json";
+        options.SwaggerEndpoint(url, name);
+    }
+});
 
 app.UseHttpsRedirection();
 app.UseCors(myAllowSpecificOrigins);
